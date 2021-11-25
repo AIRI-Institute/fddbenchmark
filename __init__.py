7b6846f3068ba14ef99b42e3a4809e8b4e99b5d7
Vitaliy Pozdnyakov <pozdnyakov.vitaliy@yandex.ru>
2021-11-25 20:53:09 +0300

    upd small_tep downloader

--- __init__.py
+++ __init__.py
@@ -1,18 +1,30 @@
 import requests
 import pandas as pd
+import numpy as np
+from tqdm import tqdm
 
-class FDDDataset(pd.DataFrame):
-    def __init__(self, name):
-        available_datasets = ['small_tep_train', 'small_tep_test']
+class FDDDataset():
+    def __init__(self, name, splitting_type='supervised'):
+        self.splitting_type = splitting_type
+        self.df = None
+        self.labels = None
+        self.train_mask = None
+        self.test_mask = None
+        available_datasets = ['small_tep']
         available_datasets_str = ', '.join(available_datasets)
-        if name == 'small_tep_train':
-            url = 'https://raw.githubusercontent.com/airi-industrial-ai/fdd-datasets/main/small_tep/training.csv'
-            open('training.csv', 'wb').write(requests.get(url).content)
-            df = pd.read_csv('training.csv')
-        elif name == 'small_tep_test':
-            url = 'https://raw.githubusercontent.com/airi-industrial-ai/fdd-datasets/main/small_tep/testing.csv'
-            open('testing.csv', 'wb').write(requests.get(url).content)
-            df = pd.read_csv('testing.csv')
+        if name == 'small_tep':
+            self.load_small_tep()
         else:
             raise Exception(f'{name} is an unknown dataset. Available datasets are: {available_datasets_str}')
-        super().__init__(data=df)
+    def load_small_tep(self):
+        if self.splitting_type == 'supervised':
+            file_names = ['dataset.csv', 'labels.txt', 'train_mask.txt', 'test_mask.txt']
+            for fname in tqdm(file_names):
+                url = f'https://raw.githubusercontent.com/airi-industrial-ai/fdd-datasets/main/small_tep/{fname}'
+                open(fname, 'wb').write(requests.get(url).content)
+            self.df = pd.read_csv('dataset.csv')
+            self.labels = np.loadtxt('labels.txt', dtype=int)
+            self.train_mask = np.loadtxt('train_mask.txt', dtype=bool)
+            self.test_mask = np.loadtxt('test_mask.txt', dtype=bool)
+    def _repr_html_(self):
+        return self.df.head()._repr_html_()
---
+++ config.md
@@ -0,0 +1,21 @@
+## Splitting types
+
+### Supervised
+train set: all types, test set: all types
+
+Tasks: fault detection, fault diagnosis
+
+### Unsupervised
+train set: normal type, test set: all types
+
+Tasks: fault detection, fault clustering
+
+### Semi-supervised
+train set: all types, test set: all types
+
+in train set, in each fault type, only a single run is labeled
+
+Tasks: fault detection, fault diagnosis
+
+## Input types
+single instant, regular slise, rolling window
