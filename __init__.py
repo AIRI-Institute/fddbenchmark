54abcbd649b04933224e8d3ed630f34d2eb27537
Vitaliy Pozdnyakov <pozdnyakov.vitaliy@yandex.ru>
2021-11-25 21:41:05 +0300

    add unsupervised and semisupervised types

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ import numpy as np
 from tqdm import tqdm
 
 class FDDDataset():
-    def __init__(self, name, splitting_type='supervised'):
+    def __init__(self, name, splitting_type):
         self.splitting_type = splitting_type
         self.df = None
         self.labels = None
@@ -18,13 +18,29 @@ class FDDDataset():
             raise Exception(f'{name} is an unknown dataset. Available datasets are: {available_datasets_str}')
     def load_small_tep(self):
         if self.splitting_type == 'supervised':
-            file_names = ['dataset.csv', 'labels.txt', 'train_mask.txt', 'test_mask.txt']
-            for fname in tqdm(file_names):
-                url = f'https://raw.githubusercontent.com/airi-industrial-ai/fdd-datasets/main/small_tep/{fname}'
-                open(fname, 'wb').write(requests.get(url).content)
+            download_files(['dataset.csv', 'labels.csv', 'train_mask.txt', 'test_mask.txt'])
             self.df = pd.read_csv('dataset.csv')
-            self.labels = np.loadtxt('labels.txt', dtype=int)
+            self.labels = pd.read_csv('labels.csv')
             self.train_mask = np.loadtxt('train_mask.txt', dtype=bool)
             self.test_mask = np.loadtxt('test_mask.txt', dtype=bool)
+        if self.splitting_type == 'unsupervised':
+            download_files(['dataset.csv', 'labels.csv', 'test_mask.txt', 'normal_train_mask.txt'])
+            self.df = pd.read_csv('dataset.csv')
+            self.labels = pd.read_csv('labels.csv')
+            self.train_mask = np.loadtxt('normal_train_mask.txt', dtype=bool)
+            self.test_mask = np.loadtxt('test_mask.txt', dtype=bool)
+        if self.splitting_type == 'semisupervised':
+            download_files(['dataset.csv', 'labels.csv', 'test_mask.txt', 'train_mask.txt', 'unlabeled_train_mask.txt'])
+            self.df = pd.read_csv('dataset.csv')
+            self.labels = pd.read_csv('labels.csv')
+            self.train_mask = np.loadtxt('train_mask.txt', dtype=bool)
+            self.test_mask = np.loadtxt('test_mask.txt', dtype=bool)
+            unlabeled_train_mask = np.loadtxt('unlabeled_train_mask.txt', dtype=bool)
+            self.labels.loc[unlabeled_train_mask, ['fault_type']] = None
     def _repr_html_(self):
         return self.df.head()._repr_html_()
+
+def download_files(file_names):
+    for fname in tqdm(file_names, desc='Downloading'):
+        url = f'https://raw.githubusercontent.com/airi-industrial-ai/fdd-datasets/main/small_tep/{fname}'
+        open(fname, 'wb').write(requests.get(url).content)
