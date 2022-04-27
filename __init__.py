b4810fc875aadc4ea72ec272e147c4ec340b895e
Vitaliy Pozdnyakov <pozdnyakov.vitaliy@yandex.ru>
2022-04-27 18:00:22 +0300

    add acc

--- __init__.py
+++ __init__.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics.cluster import normalized_mutual_info_score, adjusted_rand_score
+from scipy.optimize import linear_sum_assignment as linear_assignment
 from tqdm import tqdm
 import zipfile
 import os
@@ -250,6 +251,7 @@ class FDDEvaluator():
         metrics['diagnosis']['CDR_total'] = correct_diagnoses.sum() / tp
         metrics['diagnosis']['MDR'] = (tp - correct_diagnoses.sum()) / tp
         
+        metrics['clustering']['ACC'] = cluster_acc(labels, pred)
         metrics['clustering']['NMI'] = normalized_mutual_info_score(labels, pred)
         metrics['clustering']['ARI'] = adjusted_rand_score(labels, pred)
         return metrics
@@ -269,3 +271,24 @@ class FDDEvaluator():
         print('\nClustering metrics\n-----------------')
         print('Adjusted Rand Index (ARI): {:.4f}'.format(metrics['clustering']['NMI']))
         print('Normalized Mutual Information (NMI): {:.4f}'.format(metrics['clustering']['ARI']))
+        print('Unsupervised Clustering Accuracy (ACC): {:.4f}'.format(metrics['clustering']['ACC']))
+
+def cluster_acc(y_true, y_pred):
+    """
+    Calculate clustering accuracy. Require scikit-learn installed. 
+    Taken from https://github.com/XifengGuo/DEC-DA
+    # Arguments
+        y: true labels, numpy.array with shape (n_samples,)
+        y_pred: predicted labels, numpy.array with shape (n_samples,)
+    # Return
+        accuracy, in [0,1]
+    """
+    y_true = y_true.astype(np.int64)
+    assert y_pred.size == y_true.size
+    D = max(y_pred.max(), y_true.max()) + 1
+    w = np.zeros((D, D), dtype=np.int64)
+    for i in range(y_pred.size):
+        w[y_pred[i], y_true[i]] += 1
+    ind = linear_assignment(w.max() - w)
+    
+    return sum([w[ind[0][i], ind[1][i]] for i in range(ind[0].size)]) * 1.0 / y_pred.size
