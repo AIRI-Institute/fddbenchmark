870f9cca8116128a416618e0336c0994e397335f
Vitaliy Pozdnyakov <pozdnyakov.vitaliy@yandex.ru>
2023-10-17 18:30:08 +0300

    Update setup.py to 0.0.3

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='fddbenchmark',
-    version='0.0.2',    
+    version='0.0.3',    
     description='Benchmarking fault detection and diagnosis methods',
     url='https://github.com/airi-industrial-ai/fddbenchmark',
-    author='Vitaliy Pozdnyakov',
+    author='Vitaliy Pozdnyakov, Mikhail Goncharov, Platon Ivanov'
     author_email='pozdnyakov@airi.net',
     license='MIT License',
     packages=['fddbenchmark'],
